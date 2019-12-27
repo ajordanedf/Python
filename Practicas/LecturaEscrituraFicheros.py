4d7d3c0ad89fcb2adae1db8478b1d6164d58e685
Toni <ajordan@entornosdeformacion.com>
2019-12-27 19:54:47 +0100

    Lectura y escritura de ficheros recordada

--- Practicas/LecturaEscrituraFicheros.py
+++ Practicas/LecturaEscrituraFicheros.py
@@ -3,18 +3,34 @@
 #Author: Toni
 
 
-archivo= open("./ArchivosCreadosPorPython/Archivo1.txt", "w")
-nombre= input("Nombre: ")
-Apellidos= input("Apellidos: ")
-Edad= input("Edad: ")
-Pais= input("Pais: ")
+# archivo= open("./ArchivosCreadosPorPython/Archivo1.txt", "w")
+# nombre= input("Nombre: ")
+# Apellidos= input("Apellidos: ")
+# Edad= input("Edad: ")
+# Pais= input("Pais: ")
 
-archivo.write("\n" + nombre + "\n")
-archivo.write(Apellidos + "\n")
-archivo.write(Edad + "\n")
-archivo.write(Pais + "\n")
+# archivo.write("\n" + nombre + "\n")
+# archivo.write(Apellidos + "\n")
+# archivo.write(Edad + "\n")
+# archivo.write(Pais + "\n")
 
-print("Pos oiga señora, esto funciona")
+# print("Pos oiga señora, esto funciona")
+
+# archivo.close()
+
+# archivo= open("./ArchivosCreadosPorPython/Archivo1.txt", "r")
+
+# for l in archivo.read().split("\n"):
+#     print(l)
+
+# print("Pos oiga señora, esto funciona")
+
+# archivo.close()
+
+
+archivo= open("./ArchivosCreadosPorPython/Archivo1.txt", "a")
+
+archivo.write(input("\nDedicacion: "))
+archivo.write(input("\nEstatura: "))
 
 archivo.close()
-
