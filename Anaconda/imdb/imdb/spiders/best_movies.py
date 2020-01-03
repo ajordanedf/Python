7044bff3fa0ad02c018197f4f92e7a7652f784bd
unknown <Toni@LENI20180053.upvnet.upv.es>
2020-01-03 14:10:56 +0100

    Nueva plantilla rellenada

--- Anaconda/imdb/imdb/spiders/best_movies.py
+++ Anaconda/imdb/imdb/spiders/best_movies.py
@@ -7,15 +7,19 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['imdb.com']
-    start_urls = ['http://imdb.com/']
+    start_urls = ['https://www.imdb.com/search/title/?genres=drama&groups=top_250&sort=user_rating,desc']
 
+    #Nuevo la plantilla, dice a que página acceder, hacer mayor incapié luego
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths="//h3[@class='lister-item-header']//a"), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield{
+            "title" : response.xpath("//div[@class='title_wrapper']/h1[1]/text()").get(),
+            "Año" : response.xpath("//span[@id='titleYear']/a/text()").get(),
+            "Duracion" : response.xpath('normalize-space(//div[@class="subtext"]/time/text())').get(),
+            "genero" : response.xpath("//div[@class='subtext']/a[1]/text()").get(),
+            "rating" : response.xpath("//strong/span[@itemprop]/text()").get(),
+            "Url-pelicula" : response.url
+        }
